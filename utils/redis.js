fe6ef51ee3020a980f26babfd2461347bf21d71f
victorcreativee <tuyizerevictor@gmail.com>
2025-06-24 16:11:11 +0200

    check

---
+++ utils/redis.js
@@ -0,0 +1,32 @@
+// utils/redis.js
+import { createClient } from 'redis';
+import { promisify } from 'util';
+
+class RedisClient {
+    constructor() {
+        this.client = createClient();
+        this.client.on('error', (err) => console.error('Redis error:', err));
+        this.getAsync = promisify(this.client.get).bind(this.client);
+        this.setAsync = promisify(this.client.set).bind(this.client);
+        this.delAsync = promisify(this.client.del).bind(this.client);
+    }
+
+    isAlive() {
+        return this.client.connected;
+    }
+
+    async get(key) {
+        return this.getAsync(key);
+    }
+
+    async set(key, value, duration) {
+        await this.setAsync(key, value, 'EX', duration);
+    }
+
+    async del(key) {
+        await this.delAsync(key);
+    }
+}
+
+const redisClient = new RedisClient();
+export default redisClient;
