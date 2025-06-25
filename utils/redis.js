ff15728ed26d34ea9a1cf262e7e0ac18d6d33def
victorcreativee <tuyizerevictor@gmail.com>
2025-06-25 16:00:02 +0200

    eslint fix

--- utils/redis.js
+++ utils/redis.js
@@ -1,53 +1,44 @@
-import redis from 'redis';
+import { createClient } from 'redis';
+import { promisify } from 'util';
 
 class RedisClient {
-    constructor() {
-        this.client = redis.createClient();
+  constructor() {
+    this.client = createClient();
 
-        this.client.on('error', (err) => {
-            // use console.error for real errors is acceptable in some setups, but Airbnb prefers none
-        });
+    this.client.on('error', (err) => {
+      console.error('Redis Client Error:', err);
+    });
 
-        this.client.on('connect', () => {
-            this.connected = true;
-        });
-    }
+    this.connected = true;
 
-    isAlive() {
-        return this.client.connected === true;
-    }
+    this.client.on('connect', () => {
+      this.connected = true;
+    });
 
-    get(key) {
-        return new Promise((resolve) => {
-            this.client.get(key, (err, value) => {
-                if (err) {
-                    resolve(null);
-                } else {
-                    resolve(value);
-                }
-            });
-        });
-    }
+    this.client.on('end', () => {
+      this.connected = false;
+    });
 
-    set(key, value, duration) {
-        return new Promise((resolve) => {
-            this.client.setex(key, duration, value, (err) => {
-                if (err) {
-                    resolve();
-                } else {
-                    resolve();
-                }
-            });
-        });
-    }
+    this.getAsync = promisify(this.client.get).bind(this.client);
+    this.setAsync = promisify(this.client.setex).bind(this.client);
+    this.delAsync = promisify(this.client.del).bind(this.client);
+  }
 
-    del(key) {
-        return new Promise((resolve) => {
-            this.client.del(key, (err) => {
-                resolve();
-            });
-        });
-    }
+  isAlive() {
+    return this.connected;
+  }
+
+  async get(key) {
+    return this.getAsync(key);
+  }
+
+  async set(key, value, duration) {
+    return this.setAsync(key, duration, value);
+  }
+
+  async del(key) {
+    return this.delAsync(key);
+  }
 }
 
 const redisClient = new RedisClient();
